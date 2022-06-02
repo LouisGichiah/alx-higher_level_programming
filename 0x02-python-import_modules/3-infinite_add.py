e68e793ef2b96612706a0c29f13f77e68f34fedf
LouisGichiah <louiegpaul@gmail.com>
2022-06-02 21:26:39 +0300

    Who are you

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys
     result = 0
-    for i in range(1, len(sys.argv)):
-        result += int(sys.argv[i])
+    for a in range(1, len(sys.argv)):
+        result += int(sys.argv[a])
     print("{}".format(result))
