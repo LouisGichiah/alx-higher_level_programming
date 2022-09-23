a2b5efd335f00a9d652f27f087f0156d96ae3512
LouisGichiah <louiegpaul@gmail.com>
2022-09-24 01:19:36 +0300

    corrections

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-""" Python script that fetches https://intranet.hbtn.io/status """
-import urllib.request
+"""Fetches https://intranet.hbtn.io/status"""
 
 
 if __name__ == "__main__":
+    import urllib.request
+
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
-<<<<<<< HEAD
-        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
-=======
         print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
->>>>>>> 85119f6aa2bc3db6b492be401bca71fa8e6485fb
